628878412502963aba7fbe945a8378eb3bf1bea2
Sean <sean@codekoalas.com>
2016-09-06 09:56:26 -0500

    Redux tutorial, learning about Reducers

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,9 @@
+// A Reducer is a function that returns a piece of the applications state
+export default function () {
+  return [
+    { title: 'Javascript: The Good Parts' },
+    { title: 'Eloquent Javascript' },
+    { title: 'Good to Great' },
+    { title: 'Pragmatic Programmer' }    
+  ]
+}
